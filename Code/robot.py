4eee3ff2d08c296d106d8477a47f10d7ce56aa5c
Kevin Dixon <kevinrdixon@gmail.com>
2016-09-24 21:20:18 +0100

    Add obstacle detection

--- Code/robot.py
+++ Code/robot.py
@@ -14,6 +14,16 @@ class Robot:
     # Setting the duty cycle to 0 means the motors will not turn
     STOP = 0
 
+    # Line following
+
+    # Set variables for the GPIO pins
+    PIN_LINE_FOLLOWER = 25
+
+    # Ultrasound
+    # Define GPIO pins to use on the Pi
+    PIN_ULTRASOUND_TRIGGER = 17
+    PIN_ULTRASOUND_ECHO = 18
+
     def __init__(self):
         # Set the GPIO modes
         GPIO.setmode(GPIO.BCM)
@@ -36,6 +46,15 @@ class Robot:
         self.pwmMotorABackwards.start(Robot.STOP)
         self.pwmMotorBForwards.start(Robot.STOP)
         self.pwmMotorBBackwards.start(Robot.STOP)
+
+        # Line following
+        # Set pin 25 as an input so its value can be read
+        GPIO.setup(Robot.PIN_LINE_FOLLOWER, GPIO.IN)
+
+        # Ultrasound
+        # Set pins as output and input
+        GPIO.setup(Robot.PIN_ULTRASOUND_TRIGGER, GPIO.OUT) # Trigger
+        GPIO.setup(Robot.PIN_ULTRASOUND_ECHO, GPIO.IN) # Echo
 
     def __del__(self):
         self.stopMotors()
@@ -95,80 +114,48 @@ class Robot:
         self.pwmMotorBForwards.ChangeDutyCycle(Robot.STOP)
         self.pwmMotorBBackwards.ChangeDutyCycle(self.dutyCycleB)
 
-#
-# Line follower control
-#
+    #
+    # Line follower
+    #
 
-# Set variables for the GPIO pins
-pinLineFollower = 25
-'''
-# Set pin 25 as an input so its value can be read
-GPIO.setup(pinLineFollower, GPIO.IN)
-try:
-  # Repeat the next indented block forever
-  while True:
-    # If the sensor is Low (=0), it is above the black line
-    if GPIO.input(pinLineFollower)==0:
-      print('The sensor is seeing a black surface')
-    # If not (else), print the following
-    else:
-      print('The sensor is seeing a white surface')
-    # Wait, then do the same again
-    time.sleep(0.2)
-# If you press CTRL+C, cleanup and stop
-except KeyboardInterrupt:
- GPIO.cleanup()
-'''
+    def getLineFollowSensorValue(self):
+        return GPIO.input(Robot.PIN_LINE_FOLLOWER)
 
-#
-# Ultrasonic
-#
+    def lineFollowerOnBlack(self):
+        return self.getLineFollowSensorValue() == 0
 
-'''# Define GPIO pins to use on the Pi
-pinTrigger = 17
-pinEcho = 18
-print("Ultrasonic Measurement")
-# Set pins as output and input
-GPIO.setup(pinTrigger, GPIO.OUT) # Trigger
-GPIO.setup(pinEcho, GPIO.IN) # Echo
+    #
+    # Ultrasonic
+    #
 
-try:
-  # Repeat the next indented block forever
-  while True:
+    def getObstacleDistance(self):
       # Set trigger to False (Low)
-      GPIO.output(pinTrigger, False)
+      GPIO.output(Robot.PIN_ULTRASOUND_TRIGGER, False)
       # Allow module to settle
       time.sleep(0.5)
       # Send 10us pulse to trigger
-      GPIO.output(pinTrigger, True)
+      GPIO.output(Robot.PIN_ULTRASOUND_TRIGGER, True)
       time.sleep(0.00001)
-      GPIO.output(pinTrigger, False)
+      GPIO.output(Robot.PIN_ULTRASOUND_TRIGGER, False)
       # Start the timer
       StartTime = time.time()
       # The start time is reset until the Echo pin is taken high (==1)
-      while GPIO.input(pinEcho)==0:
+      while GPIO.input(Robot.PIN_ULTRASOUND_ECHO)==0:
           StartTime = time.time()
       # Stop when the Echo pin is no longer high - the end time
-      while GPIO.input(pinEcho)==1:
+      while GPIO.input(Robot.PIN_ULTRASOUND_ECHO)==1:
           StopTime = time.time()
           # If the sensor is too close to an object, the Pi cannot
           # see the echo quickly enough, so it has to detect that
           # problem and say what has happened
           if StopTime-StartTime >= 0.04:
-              print("Hold on there! You're too close for me to see.")
-              StopTime = StartTime
-              break
+            return 0
       # Calculate pulse length
-      ElapsedTime = StopTime - StartTime
+      elapsedTime = StopTime - StartTime
       # Distance pulse travelled in that time is
       # time multiplied by the speed of sound (cm/s)
-      Distance = ElapsedTime * 34326
+      distance = elapsedTime * 34326
       # That was the distance there and back so halve the value
-      Distance = Distance / 2
-      print("Distance: %.1f cm" % Distance)
-      time.sleep(0.5)
-# If you press CTRL+C, cleanup and stop
-except KeyboardInterrupt:
-  # Reset GPIO settings
-  GPIO.cleanup()
-'''
+      distance = distance / 2
+      return distance
+
